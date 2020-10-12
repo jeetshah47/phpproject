367ca27220c26c1e3ee626a879be97e850cb37c0
Jeet shah <jeetshahajwa@gmail.com>
2020-10-12 18:30:50 +0530

    Update back.php

---
+++ back.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "sql12.freemysqlhosting.net";
+$username = "sql12370284";
+$password = "p3ABPUKQxT";
+$dbname = "sql12370284";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+// Sign up variables
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$stmt =  $conn->prepare("INSERT INTO users (user_name, user_uname, passwords) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $name, $username, $passwords);
+
+$name = $_POST["name"];
+$username = $_POST["username"];
+$passwords = $_POST["password"];
+
+$stmt->execute();
+
+echo "New records created successfully";
+$stmt->close();
+mysqli_close($conn);
+?>

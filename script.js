b3455aed0e86ed85e51d9ac5747fbc1b8bec921e
Jeet shah <jeetshahajwa@gmail.com>
2020-10-12 20:56:46 +0530

    added google sign in

--- script.js
+++ script.js
@@ -19,3 +19,10 @@ const backPreview = () => {
     document.getElementById("pre1").style.visibility = "visible";
     document.getElementById("pre2").style.visibility = "hidden";
 }
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}

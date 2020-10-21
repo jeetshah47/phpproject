717049df70dfecbcc47fa1db31139ac8af0d8f01
root <root@localhost.localdomain>
2020-10-21 19:49:42 +0530

    more changes

---
+++ user_session/user_session.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+    <title>Session</title>
+    <link href = "session_style.css" rel="stylesheet">
+    </head>
+    <body>
+     <div class="hero_section">
+        <div class="hero_text">
+            <h1>Project Blog</h1>
+            <p>Find Interesting Project For Your Business</p>
+            
+        </div>
+     </div>
+     <div class="project_section"></div>
+     <div class="about_section"></div>
+     <div class="extras_section"></div>
+
+    </body>
+</html>

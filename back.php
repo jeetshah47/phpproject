5b8df21a4ef8051491aefb16d73059a0a9c5ea2e
Jeet shah <jeetshahajwa@gmail.com>
2020-10-28 07:45:08 +0530

    database  service updated

--- back.php
+++ back.php
@@ -3,13 +3,13 @@
 <script src="script.js"></script>
 <?php 
 session_start();
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "iinvest";
+$servername = "mysql57-bf63.euw2.cloud.ametnes.com";
+$username = "PaRjujvbCT";
+$dbpass = "suvjz2xh5litv9WE2LtB";
+$database = "1939748071";
 
 // Create connection
-$conn = mysqli_connect("localhost", "root", "", "iinvest");
+$conn = mysqli_connect($servername,$username ,$dbpass,$database);
 // Sign up variables
 // Check connection
 if($conn === FALSE) {
---
+++ op.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php 
+$img_url = "uploads/map.jpg";
+?>
+
+<img src = "<?php echo $img_url ?>" >
+</body>
+</html>

336f6b22c8162c712abb225745ee4cf71873d076
Jeet shah <jeetshahajwa@gmail.com>
2020-10-30 14:09:25 +0530

    Update login.php

--- login.php
+++ login.php
@@ -21,7 +21,7 @@ if (mysqli_num_rows($result) > 0) {
         $flag = 1;
         session_start();
         $_SESSION["user"] = $row["user_name"];
-        echo "<script>location.href = window.open('user_session/user_home.php','_self') </script>";
+        echo "<script>location.href ='user_session/user_home.php';</script>";
         break;
     } 
   }
